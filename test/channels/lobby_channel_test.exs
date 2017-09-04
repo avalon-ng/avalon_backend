fcf74c32994c7e3da42051fee147dc920c522bf4
jeno5980515 <jeno5980515@yahoo.com.tw>
2017-09-04 14:40:36 +0800

    add login lobby channel test

--- test/channels/lobby_channel_test.exs
+++ test/channels/lobby_channel_test.exs
@@ -2,9 +2,17 @@ defmodule AvalonBackend.LobbyChannelTest do
   use AvalonBackend.ChannelCase
   alias AvalonBackend.UserSocket
   alias AvalonBackend.LobbyChannel
+  alias AvalonBackend.UserModel
 
-  test "user receives the current list of users online" do
+  def connectLobby() do
     {:ok, socket} = connect(UserSocket, %{}) 
     {:ok, _, socket} = subscribe_and_join(socket, "lobby")
+    socket
   end
+
+  test "user connect and update user model" do
+    socket = connectLobby()
+    assert socket.id !== "" && socket.id !== nil && IO.inspect UserModel.get(socket.id) !== nil
+  end
+  
 end
